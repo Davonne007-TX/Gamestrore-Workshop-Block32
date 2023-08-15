9c20c0c337b99fd2661da8a37885ffb3c1bd7699
Vonney <davonnejv@gmail.com>
2023-08-14 20:09:46 -0500

    adding more notes

--- index.js
+++ index.js
@@ -8,23 +8,27 @@ app.use(morgan('dev'));  //also a middleware, also applied to every route
 
 // init body-parser
 const bodyParser = require('body-parser');
-app.use(bodyParser.json()); //this will run every response, middleware, every app.use
+app.use(bodyParser.json()); //this will run every response into json, middleware, every app.use
 
 // init cors
 const cors = require('cors');  //being used on every route, middleware 
 app.use(cors());
 
 // init db client
-const client = require('./db/client');
-client.connect();
+const client = require('./db/client');  
+client.connect();  //connecting to the client, good place to do it here in the index 
 
 app.get('/', (req, res) => {
     res.send('Hello World!');
 });
 
 // Router: /api
-app.use('/api', require('./api'));
+app.use('/api', require('./api')); //any route that are exported from here, all routes will start with api
 
 app.listen(PORT, () => {
     console.log(`Server listening on port ${PORT}`);
 });
+
+
+//this is the outer most index, where we initiate our app, the invocation of the express library
+ //express lets us spin up a server, to make build an API 
