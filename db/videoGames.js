c64261fe1b396ab488c95f941f7bc4713c57ec97
Vonney <davonnejv@gmail.com>
2023-08-15 07:17:22 -0500

    trying to add the function for createVideoGame

--- db/videoGames.js
+++ db/videoGames.js
@@ -8,8 +8,8 @@ const query = `
 // GET - /api/video-games - get all video games    
 async function getAllVideoGames() {
     try {
-        const { rows: videoGames } = await client.query(query);
-        return videoGames;
+        const { rows } = await client.query(query);
+        return rows;
     } catch (error) {
         throw new Error("Make sure you have replaced the REPLACE_ME placeholder.")
     }
@@ -32,47 +32,19 @@ async function getVideoGameById(id) {
   //try catch
   //learn how to create, add to a database
   //return something 
-  //catch error
 async function createVideoGame(body) {
-   try {
-        const { rows: [videoGame] } = await client.query(`
-            INSERT INTO videoGames (name, description, price, isPopular, imgUrl)
-            VALUES ('Cobra Kai', 'karate martial arts', 60, true, "https://i.postimg.cc/rskdPVVL/thao-lee-Xl-il-WBKJNk-unsplash.jpg")
-            RETURNING *;
-        `, [body])
-        return videoGame;
-
-   } catch (error) {
-    throw error
-   }
+    const {name, description, price, inStock, isPopular, imgUrl } = body;
+    try {
+        const { rows: [videoGames]} = await client.query(`
+            INSERT INTO videoGames(name, description, price, "inStock", "isPopular", "imgUrl") 
+            VALUES($1, $2, $3, $4, $5, $6)
+            SELECT * FROM videoGames;
+        `, [name, description, price, inStock, isPopular, imgUrl])
+        
+    } catch (error) {
+        throw error
+    }
 }
-
-// async function createVideoGame(body) {
-//     const { name, description, price, inStock, isPopular, imgUrl } = body
-//    try {
-//         const { rows: [videoGame] } = await client.query(`
-//             INSERT INTO videoGames (name, description, price, inStock, isPopular, imgUrl)
-//             VALUES ($1, $2, $3, $4, $5, $6)
-//             RETURNING *
-//         `, [name, description, price, inStock, isPopular, imgUrl])
-
-//         const newGameInfo = {
-//             name: "Cobra Kai",
-//             description: "Karate martial arts",
-//             price: 60.99,
-//             inStock: true,
-//             isPopular: true,
-//             imgUrl: "https://i.postimg.cc/rskdPVVL/thao-lee-Xl-il-WBKJNk-unsplash.jpg"
-//         }
-
-//         const newVideoGame = await createVideoGame(newGameInfo);
-//         console.log("Game added:", newVideoGame)
-
-//         return videoGame;
-//    } catch (error) {
-//     throw error
-//    }
-// }
 
 
 // PUT - /api/video-games/:id - update a single video game by id
@@ -93,7 +65,7 @@ async function updateVideoGame(id, fields = {}) {
 //catch error
 async function deleteVideoGame(id) {
     try {
-        const { rows: videoGame } = await client.query( `
+        const { rows: [videoGame] } = await client.query( `
             DELETE FROM videoGames
             WHERE id = $1;
         `, [id])
